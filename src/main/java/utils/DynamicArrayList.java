cda1f18981a81ef8acef2fa041cf4e0f241e05c3
Cal Woods <D00270955@student.dkit.ie>
2025-02-10 19:46:49 +0000

    Updated DynamicArrayList by adding indexOf(String value) that gets a position containing a provided value. If not found, returns -1.

--- src/main/java/utils/DynamicArrayList.java
+++ src/main/java/utils/DynamicArrayList.java
@@ -40,6 +40,8 @@ public class DynamicArrayList {
      * Gives a value at a given position within the bounds of instance size.
      * @param position Given position in DynamicArrayList
      * @return The value at the given position
+     *
+     * @throws IndexOutOfBoundsException If given position is less than 0 or greter than size of DynamicArrayList.
      */
     public String get(int position) {
         //Validation - Check for a position less than 0 as it is not a valid position in an array, or a position greater than numElements as anything beyond this is invalid
@@ -50,5 +52,22 @@ public class DynamicArrayList {
 
         //Return array value at given position
         return this.data[position];
+    }
+
+    /**
+     * Tries to find the position of a given value in DynamicArrayList instance.
+     * @param value Given String value
+     * @return an int position in DynamicArrayList or -1 if value not present.
+     */
+    public int indexOf(String value) {
+        //Validation - if given string value is null or value.length() is 0, return -1
+        if(value == null || value.length() == 0) return -1;
+        for(int i = 0; i < this.numElements; i++) {
+            if(this.data[i].equals(value)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
