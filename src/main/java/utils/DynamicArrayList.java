e0fcfc761075fc00013026e0567a69fa1bcc4deb
Cal Woods <D00270955@student.dkit.ie>
2025-02-10 11:36:39 +0000

    Added Project files.

---
+++ src/main/java/utils/DynamicArrayList.java
@@ -0,0 +1,12 @@
+package utils;
+
+/**
+ *
+ * @author michelle, Contributed by Cal Woods
+ */
+public class DynamicArrayList {
+    //Attributes
+    private String[] data;
+    private int size;
+
+}
