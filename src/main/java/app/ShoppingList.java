1a64e1944d8e3211798b9b0d2a198628d161e353
Cal Woods <D00270955@student.dkit.ie>
2025-02-11 21:35:31 +0000

    Updated 'ShoppingList' class' 'main()' method with an instance of 'DynamicArrayList' class.

--- src/main/java/app/ShoppingList.java
+++ src/main/java/app/ShoppingList.java
@@ -1,11 +1,20 @@
 package app;
 
+import utils.DynamicArrayList;
+
 /**
  *
- * @author michelle
+ * @author michelle Contributed By Cal Woods
  */
 public class ShoppingList {
     public static void main(String[] args) {
-        
+        //Print message
+        System.out.println("Welcome to the ShoppingList app.\nCreating a blank DynamicArrayList instance & printing it's contents");
+
+        //Declare DynamicArrayList instance using the default blank constructor
+        DynamicArrayList dynamicList = new DynamicArrayList();
+
+        //Print dynamicList
+        System.out.println(dynamicList);
     }
 }
