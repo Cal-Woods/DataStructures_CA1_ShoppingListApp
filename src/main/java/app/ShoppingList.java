97a0f1e6e2eac48604584487fd83a5a52265a9fc
Cal Woods <D00270955@student.dkit.ie>
2025-02-12 01:00:24 +0000

    Updated ShoppingList class by adding request for user input of how many items to add to shopping list.

--- src/main/java/app/ShoppingList.java
+++ src/main/java/app/ShoppingList.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.util.Scanner;
+
 import utils.DynamicArrayList;
 
 /**
@@ -8,13 +10,42 @@ import utils.DynamicArrayList;
  */
 public class ShoppingList {
     public static void main(String[] args) {
+        //Initialise Scanner to System.in(input)
+        Scanner sc = new Scanner(System.in);
+
+
         //Print message
-        System.out.println("Welcome to the ShoppingList app.\nCreating a blank DynamicArrayList instance & printing it's contents");
+        System.out.println("Welcome to the ShoppingList app. You will be repeatedly asked to enter item names into the shopping list. When finished, you will be offered a menu with various options until you decide to exit this app.\nCreating a blank DynamicArrayList instance & printing it's contents");
 
         //Declare DynamicArrayList instance using the default blank constructor
         DynamicArrayList dynamicList = new DynamicArrayList();
 
-        //Print dynamicList
-        System.out.println(dynamicList);
+        //Print blank lines
+        System.out.println("\n");
+
+        //print ask how many entries the user would like to make into dynamicList
+        System.out.print("How many items would you like to add to the list? ");
+        sc.nextInt();
+
+        //Clear sc buffer
+        sc.nextLine();
+
+        //TODO: Initialise while loop to show menu
+
+
+
+    }
+
+    /**
+     * Prints all valid elements in a given DynamicArrayList
+     * @param list Given DynamicArrayList
+     * @return void Nothing
+     * 
+     */
+    private static void printDynamicArrayList(DynamicArrayList list) {
+        //Print dynamicList using for loop
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(list);
+        }
     }
 }
